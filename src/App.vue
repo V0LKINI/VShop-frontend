857a2b0ba28c3aad8351011293a7afa41e1a376b
V0LKINI <k.volkov.n@gmail.com>
2023-04-17 13:02:09 +0700

    Axios и запросы на бек

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <script setup>
+import { ref, watch } from 'vue';
+import axios from "axios";
 
+const search = ref('');
+const recipes = ref([]);
+let timeoutRecipe = null;
+
+const getRecipes = async (search) => {
+  return await axios.get('recipes?search=' + search);
+}
+
+watch(search, () => {
+  clearTimeout(timeoutRecipe);
+  timeoutRecipe = setTimeout( () => {
+    getRecipes(search.value).then((r) => {
+      recipes.value = r.data.data;
+    })
+  }, 800);
+})
+
+getRecipes('').then((r) => {
+  recipes.value = r.data.data;
+})
 </script>
 
 <template>
@@ -8,6 +30,15 @@
   </header>
 
   <main>
+    <input
+        type="search"
+        v-model="search"
+        placeholder="Поиск рецептов"
+    >
+
+    <div v-for="recipe in recipes">
+      {{ recipe.text }} <br><br><br>
+    </div>
 
   </main>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './axios.js'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.mount('#app');
